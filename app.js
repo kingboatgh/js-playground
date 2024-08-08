760f2cb6a31c4a239b50bbd1a6e6b6f80c0dcd01
Kingsley Boateng <geniusboateng@gmail.com>
2024-08-08 04:51:25 +0000

    initial repo docs

---
+++ app.js
@@ -0,0 +1,41 @@
+console.log('Hello World')
+
+console.log('Hello World Page')
+
+alert('Welcome');
+
+
+// let resultC = ((1850 + 1500) * 0.1) / 100
+
+// let finalResult = Math.round(((1850 + 1500) * 0.1) / 100)
+
+// ================================
+
+// let resultC = ((1850 + 1500) * 0.2) / 100
+
+// let finalResult = Math.round(((1850 + 1500) * 0.2) / 100)
+
+
+
+// =======working with strings and numbers======== 
+
+// let check =  'Items (' + (1 + 1) + '): $' + ((2045 + 1030) / 100);
+
+// console.log(check);
+
+
+// alert(`Total cost: $${(599 + 295) / 100} \n Thank you, come again` );
+
+
+// `Items (${2 + 2}):   $${((2095 * 2) + (799 * 2))/100}`
+
+// console.log(`Items (${2 + 2}):   $${((2095 * 2) + (799 * 2))/100} \n
+// Shipping & handling: $${((4.99) + (4.99))} \n
+// Total before tax: $${(2 * 2095 + 2 * 799 + 499 + 499) / 100} \n
+// Extimated tax (10%): $${(Math.round((2 * 2095 + 2 * 799 + 499 + 499) * 0.1) / 100)}`);
+
+
+
+console.log(((499 * 2) / 100));
+// console.log(resultC);
+// console.log (finalResult)
